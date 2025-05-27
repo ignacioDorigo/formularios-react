414b3b18ead8a9a3f760bf3dd5ddf4c41d5f3274
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-27 10:44:24 -0300

    formulario componentes formik

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 // import LoginSinFormik from "./components/LoginSinFormik/LoginSinFormik";
 // import LoginConFormik from "./components/LoginConFormik/LoginConFormik";
-import LoginFormikValidacion from "./components/LoginFormikValidacion/LoginFormikValidacion";
-// import LoginFormikComponentes from "./components/LoginFormikComponentes/LoginFormikComponentes";
+// import LoginFormikValidacion from "./components/LoginFormikValidacion/LoginFormikValidacion";
+import LoginFormikComponentes from "./components/LoginFormikComponentes/LoginFormikComponentes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LoginFormikValidacion />
+        <LoginFormikComponentes />
       </header>
     </div>
   );
--- src/components/LoginFormikComponentes/LoginFormikComponentes.js
+++ src/components/LoginFormikComponentes/LoginFormikComponentes.js
@@ -1,6 +1,42 @@
 import React from "react";
 import "./LoginFormikComponentes.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 export default function LoginFormikComponentes() {
-  return <div>LoginFormikComponentes</div>;
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(formulario) => console.log(formulario)}
+    >
+      <Form className="formulario">
+        <div className="div_campo">
+          <label htmlFor="email">Email</label>
+          <Field
+            className="campo_input"
+            placeholder="Email"
+            name="email"
+            id="email"
+            type="email"
+          ></Field>
+          <ErrorMessage name="email" component="div" className="error" />
+        </div>
+
+        <div className="div_campo">
+          <label htmlFor="password">Password</label>
+          <Field
+            className="campo_input"
+            placeholder="Contraseña"
+            name="password"
+            id="password"
+            type="password"
+          ></Field>
+          <ErrorMessage name="password" component="div" className="error" />
+        </div>
+
+        <button className="btn_ingresar" type="submit">
+          Ingresar
+        </button>
+      </Form>
+    </Formik>
+  );
 }
