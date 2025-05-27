540c410f795efe94c96ca40a2b79624fcbf62a0d
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-27 00:33:53 -0300

    login con formik

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import LoginSinFormik from "./components/LoginSinFormik/LoginSinFormik";
+// import LoginSinFormik from "./components/LoginSinFormik/LoginSinFormik";
+import LoginConFormik from "./components/LoginConFormik/LoginConFormik";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LoginSinFormik />
+        <LoginConFormik />
       </header>
     </div>
   );
--- src/components/LoginConFormik/LoginConFormik.js
+++ src/components/LoginConFormik/LoginConFormik.js
@@ -1,6 +1,42 @@
 import React from "react";
-import "./LoginConFormik";
+import "./LoginConFormik.css";
+import { useFormik } from "formik";
 
 export default function LoginConFormik() {
-  return <div>LoginConFormik</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (formulario) => {
+      console.log(formulario);
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit} className="formulario">
+      <div className="div_campo">
+        <label htmlFor="email">Email</label>
+        <input
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          type="email"
+        ></input>
+      </div>
+      <div className="div_campo">
+        <label htmlFor="password">Password</label>
+        <input
+          placeholder="Contraseña"
+          id="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          type="password"
+        ></input>
+      </div>
+      <button type="submit" className="btn_ingresar">Ingresar</button>
+    </form>
+  );
 }
